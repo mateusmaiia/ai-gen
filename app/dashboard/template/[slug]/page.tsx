eb5ebc5bdfe14522358406e187dff7b9f6638799
maia <matteusmaia889@gmail.com>
2024-10-04 16:57:58 -0300

    chore: react-hot-toast dependecies

--- app/dashboard/template/[slug]/page.tsx
+++ app/dashboard/template/[slug]/page.tsx
@@ -8,12 +8,13 @@ import { Textarea } from '@/components/ui/textarea'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import { runAi } from '@/actions/ai'
-import { Loader2Icon } from 'lucide-react'
-import ReactMarkdown from 'react-markdown'
+import { Loader2Icon, ArrowLeft, Copy } from 'lucide-react'
 
 import '@toast-ui/editor/dist/toastui-editor.css';
 
 import { Editor } from '@toast-ui/react-editor';
+import Link from 'next/link'
+import toast from 'react-hot-toast'
 
 export interface Template{
   name: string;
@@ -63,58 +64,81 @@ export default function Page({params}: {params: {slug: string}}) {
     }
   }
 
+  async function handleCopy(){
+    const editorInstance = editorRef.current?.getInstance()
+    const content = editorInstance.getMarkdown()
+    
+    try {
+      await navigator.clipboard.writeText(content)
+      toast.success("Content copied to clipboard")
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div className='grid grid-cols-1 md:grid-cols-3 gap-5 px-5 '>
-      <div className='col-span-1 bg-slate-100 dark:bg-slate-900 rounded-md border p-5'>
-        <div className='flex flex-col gap-3'>
-          <Image 
-            src={t.icon} 
-            alt={t.name}
-            width={50}
-            height={50}
-          />
-          <h2 className='font-medium text-lg'>{t.name}</h2>
-          <p className='text-gray-500'>{t.desc}</p>
-        </div>
+    <div>
+      <div className="flex justify-between mx-5 my-3">
+        <Link href="/dashboard">
+          <Button><ArrowLeft /> <span className='ml-2'>Back</span></Button>
+        </Link>
 
-        <form className='mt-6' onSubmit={handleSubmit}>
-          {t.form.map((item) => (
-            <div key={item.name} className='mr-2 flex flex-col gap-2 mb-7'>
-              <label  className='font-bold pb-5'>{item.label}</label>
-
-              {item.field === 'input' ? (
-                <Input
-                  name={item.name}
-                  required={item.required}
-                  onChange={e => setQuery(e.target.value)}
-                />
-              ): (
-                <Textarea 
-                  name={item.name}
-                  required={item.required}
-                  onChange={e => setQuery(e.target.value)}
-                />
-              )}
+        <Button onClick={handleCopy}>
+          <Copy /> <span className='ml-2'>Copy</span>
+        </Button>
+      </div>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-5 px-5 '>
+          <div className='col-span-1 bg-slate-100 dark:bg-slate-900 rounded-md border p-5'>
+            <div className='flex flex-col gap-3'>
+              <Image 
+                src={t.icon} 
+                alt={t.name}
+                width={50}
+                height={50}
+              />
+              <h2 className='font-medium text-lg'>{t.name}</h2>
+              <p className='text-gray-500'>{t.desc}</p>
             </div>
-          ))}
 
-          <Button type='submit' className='w-full py-6' disabled={loading}>
-            {loading ? <Loader2Icon className='animate-spin mr-2' /> : "Generate Content"}
-          </Button>
-        </form>
-      </div>
+            <form className='mt-6' onSubmit={handleSubmit}>
+              {t.form.map((item) => (
+                <div key={item.name} className='mr-2 flex flex-col gap-2 mb-7'>
+                  <label  className='font-bold pb-5'>{item.label}</label>
 
-      <div className="col-span-2">
-          <Editor 
-            ref={editorRef}
-            initValue={"Generated content will appear here."}
-            previewStyle="vertical"
-            height="600px"
-            initialValue="wysiwyg"
-            useCommandShortcut={true}
-            // onChange={() => setContent(editorRef.current?.getInstance().getMarkdown())}
-          />
-      </div>
+                  {item.field === 'input' ? (
+                    <Input
+                      name={item.name}
+                      required={item.required}
+                      onChange={e => setQuery(e.target.value)}
+                    />
+                  ): (
+                    <Textarea 
+                      name={item.name}
+                      required={item.required}
+                      onChange={e => setQuery(e.target.value)}
+                    />
+                  )}
+                </div>
+              ))}
+
+              <Button type='submit' className='w-full py-6' disabled={loading}>
+                {loading ? <Loader2Icon className='animate-spin mr-2' /> : "Generate Content"}
+              </Button>
+            </form>
+          </div>
+
+          <div className="col-span-2">
+              <Editor 
+                ref={editorRef}
+                initValue={"Generated content will appear here."}
+                previewStyle="vertical"
+                height="600px"
+                // initialValue=""
+                useCommandShortcut={true}
+                // onChange={() => setContent(editorRef.current?.getInstance().getMarkdown())}
+              />
+          </div>
+        </div>
     </div>
   )
 }
--- components/nav/top-nav.tsx
+++ components/nav/top-nav.tsx
@@ -3,6 +3,7 @@
 import { SignedIn, SignedOut, SignInButton, UserButton, useUser } from '@clerk/nextjs'
 import Link from 'next/link'
 import { ModeToggle } from './theme-toggle'
+import { Toaster } from 'react-hot-toast'
 
 
 export default function TopNav() {
@@ -12,6 +13,7 @@ export default function TopNav() {
 
     return (
     <nav className='flex justify-between items-center p-2 shadow border'>
+        <Toaster />
         <Link href="/">AI</Link>
         <div className='flex items-center'>
             {isSignedIn && <Link href="/dashboard" className='mr-2'>{`${user.fullName}'s Dashboard`}</Link>}
