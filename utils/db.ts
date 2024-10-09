183bd9cd2c119a6048831509e002151f2486319e
Maia <matteusmaia889@gmail.com>
2024-10-09 14:54:52 -0300

    chore(db): connect db try catch

--- utils/db.ts
+++ utils/db.ts
@@ -3,5 +3,10 @@ import mongoose from 'mongoose';
 export default async function db(){
     if(mongoose.connection.readyState >= 1) return;
 
-    await mongoose.connect(process.env.DATABASE as string)
+    try {
+      await mongoose.connect(process.env.DATABASE as string)
+      console.log("Sucesso ao conectar no banco de dados")
+    } catch (error) {
+      throw new Error("throw new Error db function erro ao conectar no banco de dados: " + error)
+    }
 }
