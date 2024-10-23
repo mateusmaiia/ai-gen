8d768869b33359c185679655248cc0d6d8af6554
Maia <matteusmaia889@gmail.com>
2024-10-23 12:56:49 -0300

    route: gen-ai

---
+++ app/gen-ai/page.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import {runAi} from "@/actions/ai"
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { useState } from 'react'
+import ReactMarkdown from 'react-markdown'
+export default function Home() {
+  const [response, setResponse ] = useState("")
+  const [loading, setLoading ] = useState(false)
+  const [query, setQuery ] = useState("")
+
+  const handleClick = async (e) => {
+    e.preventDefault()
+    setLoading(true)
+
+    try {
+      const data = await runAi(query)
+      setResponse(data)
+    } catch (error) {
+      console.log(error)
+    }finally{
+      setLoading(false)
+    }
+  }
+
+  return (
+    <div className='m-5'>
+      <form onSubmit={handleClick}>
+        <Input 
+          className='mb-5' 
+          placeholder='Ask anything!' 
+          value={query} 
+          onChange={e => setQuery(e.target.value)}
+        />
+
+        <Button>Generate with AI</Button>
+      </form>
+
+
+      <Card className='mt-5'>
+        <CardHeader>AI Response will appear here...</CardHeader>
+        <CardContent>
+            {loading ? <div>Loading...</div> : (
+              <ReactMarkdown>
+                {response}
+              </ReactMarkdown>
+            )}
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+ 
